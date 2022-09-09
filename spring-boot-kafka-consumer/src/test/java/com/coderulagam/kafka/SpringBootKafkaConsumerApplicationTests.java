cfd1dd679478dcafd8381c3185cf06d56448d686
dineshTharaka <coddineshtharaka@gmail.com>
2022-09-09 22:53:05 +0530

    ss

---
+++ spring-boot-kafka-consumer/src/test/java/com/coderulagam/kafka/SpringBootKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderulagam.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
